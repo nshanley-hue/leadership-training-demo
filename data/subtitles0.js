c1208d53858de74374350f79993903c3e6d5c4da
nshanley-hue <nshanley@charlotte.edu>
2026-02-11 19:37:00 -0500

    Delete data directory

--- data/subtitles0.js
+++
@@ -1,6 +0,0 @@
-(function () {
-	const subtitles = {
-			data: "WEBVTT\n\n00:00.640 --> 00:04.360\nWelcome to navigating difficult giving\n\n00:04.860 --> 00:08.600\nfeedback to underperforming employees. As leaders, our goal\n\n00:09.100 --> 00:13.520\nis not just to manage performance, but to inspire growth, trust and accountability.\n\n"
-		};
-	window["st_{EA3973B1-6B04-42CA-BC19-512C875AF6A2}"]?.("sndAsset0", subtitles);
-})();
--- data/subtitles9.js
+++
@@ -1,6 +0,0 @@
-(function () {
-	const subtitles = {
-			data: "WEBVTT\n\n00:00.240 --> 00:03.520\nLet\'s recap. Address issues early using the\n\n00:04.020 --> 00:08.320\nSBI method or Sandwich method. Stay calm and behavior focused.\n\n00:08.720 --> 00:12.480\nEncourage accountability and growth by collaborating on solutions.\n\n00:13.200 --> 00:17.160\nEvery feedback conversation is an opportunity to strengthen trust and\n\n00:17.660 --> 00:21.280\nimprove performance. So set a date to follow up and support them.\n\n"
-		};
-	window["st_{EA3973B1-6B04-42CA-BC19-512C875AF6A2}"]?.("sndAsset9", subtitles);
-})();
